076e5d03dd161f78d4ff6ee36c6b70b466413dc7
Justin Fyfe <justin@fyfesoftware.ca>
2019-12-22 21:15:47 +0630

    Updated the subscription definitions to include hl7 and improve the guard detection

--- applet/pack.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-sdb-pakr --compile --optimize --source=.\ --output=.\bin\org.santedb.mpi.pak --keyFile=..\..\..\keys\community.santesuite.net.pfx --keyPassword=..\..\..\keys\community.santesuite.net.pass --embedcert
-
