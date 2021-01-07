c365ca7ee3d58deccf25a6aa9a5cd79aff927316
Justin Fyfe <justin@fyfesoftware.ca>
2021-01-07 17:27:42 -0500

    Rev'd versions

--- pack.bat
+++ pack.bat
@@ -3,12 +3,12 @@
 
 	PUSHD applet
 		IF EXIST "manifest.xml" (
-			pakman --compile --optimize --source=.\ --output=..\bin\org.santedb.smpi.pak --keyFile=..\..\keys\community.santesuite.net.pfx --keyPassword=..\..\keys\community.santesuite.net.pass --embedcert --install
+			pakman --compile --optimize --source=.\ --output=..\bin\org.santedb.smpi.pak --keyFile=..\..\keys\community.santesuite.net.pfx --keyPassword=..\..\keys\community.santesuite.net.pass --embedcert --install --publish --publish-server=https://packages.santesuite.net
 		)
 		POPD
 
 mkdir dist
-pakman --compile --optimize --source=..\santedb-server\SanteDB.Messaging.HL7\applet --output=.\bin\org.santedb.hl7.pak --keyFile=..\keys\community.santesuite.net.pfx --keyPassword=..\keys\community.santesuite.net.pass --embedcert --install
+pakman --compile --optimize --source=..\santedb-server\SanteDB.Messaging.HL7\applet --output=.\bin\org.santedb.hl7.pak --keyFile=..\keys\community.santesuite.net.pfx --keyPassword=..\keys\community.santesuite.net.pass --embedcert --install --publish --publish-server=https://packages.santesuite.net
 
 
 pakman --compose --source=santempi.sln.xml -o dist\santempi.sln.pak --keyFile=..\keys\community.santesuite.net.pfx --embedCert --keyPassword=..\keys\community.santesuite.net.pass 
