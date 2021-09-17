631885731bab098e924d4c3895e76a70e20840e2
Justin Fyfe <justin@fyfesoftware.ca>
2021-09-17 16:18:29 -0400

    Added match summary dashboard buttons

--- applet/controllers/matches.js
+++ applet/controllers/matches.js
@@ -1,4 +1,36 @@
 angular.module('santedb').controller('MpiMatchDashboardController', ["$scope", "$rootScope", "$state", "$templateCache", "$stateParams", function ($scope, $rootScope, $state, $templateCache, $stateParams) {
 
 
+
+    /**
+     * Submit an "ignore" request for the specified relationship
+     */
+    $scope.ignore = async function(relationshipId) {
+        try {
+
+            SanteDB.display.buttonWait(`#ignore_${relationshipId}`, true);
+            // Confirm the action
+            if(!confirm(SanteDB.locale.getString("ui.mpi.matches.ignore.confirm")))
+                return;
+
+            // Retrieve the relationship
+            var relationship = new EntityRelationship(await SanteDB.resources.entityRelationship.getAsync(relationshipId));
+            if(relationship == null) {
+                toastr.error(SanteDB.locale.getString("ui.mpi.matches.ignore.notFound"));
+                return;
+            }
+            
+            // Send the MDM-ignore post
+            var ignoreResult = await SanteDB.resources.patient.removeAssociatedAsync(relationship.holder, "mdm-ignore", relationship.target);
+            toastr.success(SanteDB.locale.getString("ui.mpi.matches.ignore.success"));
+            
+        }
+        catch(e) {
+            $rootScope.handleError(e);
+        }
+        finally {
+            SanteDB.display.buttonWait(`#ignore_${relationshipId}`, false);
+        }
+    }
+
 }]);
