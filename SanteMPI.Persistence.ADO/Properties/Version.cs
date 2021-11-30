d3805b3cc3e8a8afe42637956897801d4a6a0a9c
Justin Fyfe <justin@fyfesoftware.ca>
2021-11-29 22:45:59 -0500

    BuildBot: Added release version

--- SanteMPI.Persistence.ADO/Properties/Version.cs
+++ SanteMPI.Persistence.ADO/Properties/Version.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyFileVersion("2.1.130")]
-[assembly: System.Reflection.AssemblyVersion("2.1.130")]
+[assembly: System.Reflection.AssemblyFileVersion("2.1.131")]
+[assembly: System.Reflection.AssemblyVersion("2.1.131")]
 
 
